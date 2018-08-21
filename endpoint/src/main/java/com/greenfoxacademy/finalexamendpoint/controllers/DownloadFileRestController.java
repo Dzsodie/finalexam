201b1d8d3d1077decfa724b18b90bae8089536f1
Dzsodie <drmakarazs@gmail.com>
2018-08-22 01:25:55 +0200

    FE-01 feat(README.md): add answers for status, request response and images for better understanding

--- endpoint/src/main/java/com/greenfoxacademy/finalexamendpoint/controllers/DownloadFileRestController.java
+++ endpoint/src/main/java/com/greenfoxacademy/finalexamendpoint/controllers/DownloadFileRestController.java
@@ -1,5 +1,7 @@
 package com.greenfoxacademy.finalexamendpoint.controllers;
 
+import com.greenfoxacademy.finalexamendpoint.models.Exercise;
+import com.greenfoxacademy.finalexamendpoint.services.ExerciseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
@@ -7,13 +9,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.ServletContext;
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/download")
@@ -21,6 +21,7 @@ public class DownloadFileRestController {
 
   @Autowired
   ServletContext context;
+  ExerciseService exerciseService;
 
   @RequestMapping(value = "/pdf/{fileName:.+}", method = RequestMethod.GET, produces = "application/pdf")
   public ResponseEntity<InputStreamResource> download(@PathVariable("fileName") String fileName) throws IOException {
@@ -40,6 +41,30 @@ public class DownloadFileRestController {
     ResponseEntity<InputStreamResource> response = new ResponseEntity<>(
             new InputStreamResource(pdfFile.getInputStream()), headers, HttpStatus.OK);
     return response;
+  }
+
+  @GetMapping("/rest")                           //return httpStatus.OK
+  public ResponseEntity<Exercise> getOne() {
+    return ResponseEntity.status(HttpStatus.OK).body(new Exercise());
+  }
+
+  @RequestMapping(value = "/outdated/{id}", method = RequestMethod.DELETE)      //delete one object by id in JSON
+  @ResponseBody
+  public String outdated(@PathVariable(value = "id") long id) {
+    exerciseService.deleteById(id);
+    return "deleted: " + id;
+  }
+
+  @RequestMapping(value = "/add", method = RequestMethod.PUT)      // save one object sent in JSON
+  @ResponseBody
+  public String addNewJsonObject(@ModelAttribute Exercise exercise) {
+    exerciseService.save(exercise);
+    return "added new exercise";
+  }
+
+  @GetMapping("/all")                       //get list of exercises in JSON
+  public List<Exercise> getAllExercises() {
+    return exerciseService.getExerciseList();
   }
 
 }

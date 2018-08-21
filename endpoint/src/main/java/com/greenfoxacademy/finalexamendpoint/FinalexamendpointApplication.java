b8609c1930f782746ba1acdd9bfd73a072824b4f
Dzsodie <drmakarazs@gmail.com>
2018-08-21 20:56:37 +0200

    FE-01 feat(endpoint): Add download pdf endpoint example

---
+++ endpoint/src/main/java/com/greenfoxacademy/finalexamendpoint/FinalexamendpointApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.finalexamendpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalexamendpointApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FinalexamendpointApplication.class, args);
+  }
+}
---
+++ endpoint/src/test/java/com/greenfoxacademy/finalexamendpoint/FinalexamendpointApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.finalexamendpoint;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FinalexamendpointApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
